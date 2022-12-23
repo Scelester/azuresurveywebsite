39a9109503a4b68059be8c0182d2375c46d8038c
scelester <nabinpauudel664@gmail.com>
2022-12-24 01:11:19 +0545

    afa

--- rusyweb/survey/urls.py
+++ rusyweb/survey/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     path('',views.index,name='index'),
-     path("register", views.register_request, name="register"),
+    path("login", views.login_request, name="login"),
+    path("register", views.register_request, name="register"),
 ]
--- rusyweb/survey/views.py
+++ rusyweb/survey/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render,redirect
 from .forms import NewUserForm
 from django.contrib.auth import login
 from django.contrib import messages
+from django.contrib.auth import login, authenticate #add this
+from django.contrib.auth.forms import AuthenticationForm #add this
 
 
 # Create your views here.
@@ -17,8 +19,28 @@ def register_request(request):
 			user = form.save()
 			login(request, user)
 			messages.success(request, "Registration successful." )
-			return redirect("survey:homepage")
+			return redirect("surveyapp:homepage")
 		messages.error(request, "Unsuccessful registration. Invalid information.")
 
 	form = NewUserForm()
 	return render (request=request, template_name="surveyapp/register.html", context={"register_form":form})
+
+
+def login_request(request):
+	if request.method == "POST":
+		form = AuthenticationForm(request, data=request.POST)
+		if form.is_valid():
+			username = form.cleaned_data.get('username')
+			password = form.cleaned_data.get('password')
+			user = authenticate(username=username, password=password)
+			if user is not None:
+				login(request, user)
+				messages.info(request, f"You are now logged in as {username}.")
+				return redirect("survey:index")
+			else:
+				messages.error(request,"Invalid username or password.")
+		else:
+			messages.error(request,"Invalid username or password.")
+	form = AuthenticationForm()
+	return render(request=request, template_name="surveyapp/login.html", context={"login_form":form})
+
