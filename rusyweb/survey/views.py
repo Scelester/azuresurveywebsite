a5c57c7178b41ff3c21ecdfaf2105abe0ba73459
scelester <nabinpauudel664@gmail.com>
2022-12-24 00:56:20 +0545

    afa

--- rusyweb/survey/views.py
+++ rusyweb/survey/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import NewUserForm
+from django.contrib.auth import login
+from django.contrib import messages
+
 
 # Create your views here.
 def index(request):
     return render(request, 'surveyapp/index.html')
+
+
+
+def register_request(request):
+	if request.method == "POST":
+		form = NewUserForm(request.POST)
+		if form.is_valid():
+			user = form.save()
+			login(request, user)
+			messages.success(request, "Registration successful." )
+			return redirect("survey:homepage")
+		messages.error(request, "Unsuccessful registration. Invalid information.")
+
+	form = NewUserForm()
+	return render (request=request, template_name="surveyapp/register.html", context={"register_form":form})
