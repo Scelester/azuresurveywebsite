ac986e73d080a321fa9032ee417af97a6f510fbb
scelester <nabinpauudel664@gmail.com>
2022-12-24 00:49:06 +0545

    afa

--- rusyweb/survey/admin.py
+++ rusyweb/survey/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 
 # Register your models here.
 from survey.models import Survey_answers,Survey_questions
-admin.site.register(Survey_answers)
-admin.site.register(Survey_questions)
+admin.site.register(Survey_answer)
+admin.site.register(Survey_question)
