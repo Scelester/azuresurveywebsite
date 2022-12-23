487ff31a8984955387364f62436927b0746949fb
scelester <nabinpauudel664@gmail.com>
2022-12-24 01:15:07 +0545

    tuck

--- rusyweb/survey/urls.py
+++ rusyweb/survey/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 
 from . import views
 
+app_name = "survey"
+
 urlpatterns = [
     path('',views.index,name='index'),
     path("login", views.login_request, name="login"),
