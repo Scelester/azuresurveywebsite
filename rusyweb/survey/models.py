fb2146584888712ace8689f959f8908e314017c3
scelester <nabinpauudel664@gmail.com>
2022-12-23 23:47:46 +0545

    tuck

--- rusyweb/survey/models.py
+++ rusyweb/survey/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Survey_questions(models.Model):
+    id = models.IntegerField()
+    question = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.question
+
+class Survey_answers(models.Model):
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    question = models.ForeignKey(Survey_answers, on_delete) 
+    answer = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.answer
