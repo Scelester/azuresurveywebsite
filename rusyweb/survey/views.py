13b6644be0dd047bac5c786d6aa8c128b92a2c60
scelester <nabinpauudel664@gmail.com>
2022-12-24 01:51:34 +0545

    tuck

--- rusyweb/survey/views.py
+++ rusyweb/survey/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render,redirect
 from .forms import NewUserForm
-from django.contrib.auth import login
 from django.contrib import messages
-from django.contrib.auth import login, authenticate #add this
+from django.contrib.auth import login, authenticate,logout #add this
 from django.contrib.auth.forms import AuthenticationForm #add this
 
 
